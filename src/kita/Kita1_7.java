c2849fcf3d078901aff8c573c783de62c7455f2d
tawadarei <rei.1985.59@icloud.com>
2024-08-22 22:10:07 +0900

    kita1_7変更

--- src/kita/Kita1_7.java
+++ src/kita/Kita1_7.java
@@ -3,7 +3,16 @@ package kita;
 public class Kita1_7 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	  
+	  int x, y, t;
+	  x = 3;
+	  y = 7; 
+	  t = x; // x = 3, y = 7, t = 3
+	  x = y; // x = 7, y = 7, t = 3
+	  y = t; // x = 7, y = 3, t = 3
+	  
+	  System.out.println("x=" + x + ",y=" + y);
+	  
 
 	}
 
